252e69bcd0332e59afc07a97a994204f1c30ccda
Lucas Asth <asth_lucas@hotmail.com>
2019-12-10 09:44:28 -0300

    Appends test domains for development at /etc/hosts

--- run.sh
+++ run.sh
@@ -1,3 +1,8 @@
+grep "127.0.0.1 proxy.test domain1.test domain2.test test" /etc/hosts
+if [[ $? -ne 0 ]]; then 
+  echo -e "\n127.0.0.1 proxy.test domain1.test domain2.test test" >> /etc/hosts 
+fi
+
 ./src/main/resources/initializeKeyStore.sh
 
 docker build -t proxy:0.0.1 .
@@ -9,4 +14,6 @@ docker run -d \
 --add-host domain2.test:127.0.0.1 \
 --add-host test:127.0.0.1 \
 --name reverse-proxy proxy:0.0.1
+
+
 
