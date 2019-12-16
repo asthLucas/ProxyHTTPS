ed5ed2efcf930b5bb833843a337f66c2a9ae4135
Lucas Asth <asth_lucas@hotmail.com>
2019-12-16 02:04:59 -0300

    Removes methods which were used by the older Undertow based implementation

--- src/main/java/br/com/proxy/utils/SSLContextUtils.java
+++ src/main/java/br/com/proxy/utils/SSLContextUtils.java
@@ -5,20 +5,8 @@ import java.security.SecureRandom;
 
 import javax.net.ssl.SSLContext;
 
-import io.undertow.protocols.ssl.SNIContextMatcher;
-import io.undertow.protocols.ssl.SNIContextMatcher.Builder;
-import io.undertow.protocols.ssl.SNISSLContext;
-
 public class SSLContextUtils {
 
-    public static SNISSLContext createSNISSLContext(String domain, String keyStorePath, String trustStorePath, SSLContext defaultContext) throws Exception
-    {
-    	SSLContext domainContext = createSSLContext(keyStorePath, trustStorePath);
-    	
-    	SNIContextMatcher matcher = createSNIContextMatcher(domain, domainContext, defaultContext);
-    	return new SNISSLContext(matcher);
-    }
-    
     public static SSLContext createSSLContext(String keyStorePath, String trustStorePath) throws Exception 
     {
     	KeyStore keyStore = KeyStoreUtils.createKeyStore(keyStorePath);
@@ -28,17 +16,5 @@ public class SSLContextUtils {
     	sslContext.init(KeyStoreUtils.createKeyManagers(keyStore), KeyStoreUtils.createTrustManagers(trustStore), new SecureRandom());
     	
     	return sslContext;
-    }
-    
-    private static SNIContextMatcher createSNIContextMatcher(String domain, SSLContext sslContext, SSLContext defaultContext)
-    {
-    	Builder builder = new SNIContextMatcher.Builder();
-    	builder.setDefaultContext(defaultContext);
-    	
-    	if(defaultContext == null) {
-    		builder.setDefaultContext(sslContext);
-    	}
-
-    	return builder.addMatch(domain, sslContext).build();
     }
 }
